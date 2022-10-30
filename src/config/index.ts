4a030b7c66a9d9cdc4558d9a4f592f5a2661b3c2
hoangphuc <hoangphucnb97@gmail.com>
2022-10-30 09:43:07 +0700

    Add url to config and add config.json file

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,8 +3,10 @@
  * @interface Config
  * @member {[string]} accountConfigs: list of files contains account config
  * @member {string} network: Network we will run on, maybe mainnet or testnet
+ * @member {string} url: Url of the fullnode, will be generated automatically be network config
  */
 export interface Config {
   accountConfigs: [string],
   network: string,
+  url: string,
 }
