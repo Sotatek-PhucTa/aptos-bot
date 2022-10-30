172a5c75c5813c527b07611a7321a8b0d4f49620
hoangphuc <hoangphucnb97@gmail.com>
2022-10-30 15:39:11 +0700

    Add function loadDataFromFileAsType

--- src/common/helpers/utils.ts
+++ src/common/helpers/utils.ts
@@ -1,7 +1,14 @@
+import fs from "fs";
+
 export function isEmptyValue(value: unknown): boolean {
   if (value === null || value === undefined) return true;
   if (typeof value === 'string') {
     return value === "";
   }
   return true;
+}
+
+export async function loadDataFromFileAsType<T>(filePath: string): Promise<T> {
+  const data = fs.readFileSync(filePath, 'utf-8');
+  return JSON.parse(data) as T;
 }
