36e274296708424825b823e6dc6fb86a10801045
hoangphuc <hoangphucnb97@gmail.com>
2022-10-30 10:01:23 +0700

    Parse config from config.json file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,5 @@
+import configData from "./config.json";
+
 /**
  * This is global config for program to run, make sure check those configs carefully before run application
  * @interface Config
@@ -6,7 +8,23 @@
  * @member {string} url: Url of the fullnode, will be generated automatically be network config
  */
 export interface Config {
-  accountConfigs: [string],
+  accountConfigs: string[],
   network: string,
   url: string,
 }
+
+const config: Config = ({} as any) as Config;
+
+config.accountConfigs = configData["accountConfigs"]
+config.network = configData["network"];
+if (config.network === 'mainnet') {
+  config.url = configData["mainnetUrl"] === "" ?
+    "https://fullnode.mainnet.aptoslabs.com"
+    : configData["mainnetUrl"];
+} else if (config.network === 'testnet') {
+  config.url = configData['testnetUrl'] === '' ?
+    "https://fullnode.testnet.aptoslabs.com" :
+    configData['testnetUrl'];
+} else throw Error('Invalid network config, must be "mainnet" or "testnet"');
+
+export default config;
