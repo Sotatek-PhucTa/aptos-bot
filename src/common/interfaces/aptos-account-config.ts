449e42cfc1eaf9d643539ef0201872fd41d11c87
hoangphuc <hoangphucnb97@gmail.com>
2022-10-30 09:23:35 +0700

    Add AptosAccountConfig interface

---
+++ src/common/interfaces/aptos-account-config.ts
@@ -0,0 +1,13 @@
+/**
+ * This is config for an aptos account,
+ * these configs are load from config files, defined in config.ts
+ * @interface AptosAccountConfig
+ * @member {string} address address of an account, in hex format
+ * @member {string} privateKey private key of an account, in hex format
+ * @member {gasPrice} gas price when construct tx, the higher gas price, the more chance we get nft
+ */
+interface AptosAccountConfig {
+  address: string,
+  privateKey: string,
+  gasPrice: string,
+}
